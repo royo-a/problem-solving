1151c3bb7eb0346f7ecc0fd5e5e3bcf8dfd89666
Arnab Roy <royo.arnab@gmail.com>
2023-01-11 22:56:22 -0500

    Solved O(NlogN)

---
+++ solutions/leetcode/valid-anagram.js
@@ -0,0 +1,12 @@
+/**
+ * Time complexity: O(NlogN)
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  s = s.split('').sort().join('');
+  t = t.split('').sort().join('');
+
+  return s === t;
+};
