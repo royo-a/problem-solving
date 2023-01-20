b0994875775f395e08a1f3bd9460c37efded1aec
Arnab Roy <royo.arnab@gmail.com>
2023-01-20 14:11:12 -0500

    Solved in O(n) time and space complexity

---
+++ solutions/leetcode/replace-with-greatest-element-on-right.java
@@ -0,0 +1,41 @@
+package solutions.leetcode;
+
+class Solution {
+    public int[] replaceElements(int[] arr) {
+      // Assign max as the last element of the array
+      int max = arr[arr.length - 1];
+      // Put -1 at end of array
+      arr[arr.length - 1] = -1;
+      // For all elements starting from second last element to first
+      for(int i=arr.length-2; i>=0; i--) {
+        int copy = -1;
+        // if current element is greater than copy
+        if(arr[i] > max){
+          // copy current element
+          copy = arr[i];
+        }
+        // put max element at index
+        arr[i] = max;
+        // if copy exists
+        if(copy != -1) {
+          // make max element the copy
+          max = copy;
+        }
+      }
+      
+      return arr;
+    }
+
+    // public static void main(String[] args) {
+    //     Solution solution = new Solution();
+    //     int[] result = solution.replaceElements(new int[]{17,18,5,4,6,1});
+    //     for (int i : result) {
+    //         System.out.print(i + " ");
+    //     }
+    //     System.out.println();
+    //     int[] result2 = solution.replaceElements(new int[]{400});
+    //     for (int i : result2) {
+    //         System.out.print(i + " ");
+    //     }
+    // }
+}
